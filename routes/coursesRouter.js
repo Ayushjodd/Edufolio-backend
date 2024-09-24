6c5021b77744b923acf8357ee48937ddb1dad0a6
ayushjodd <ayushkalonia2016@gmail.com>
2024-09-24 21:12:43 +0530

    init with api skeleton

---
+++ routes/coursesRouter.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+const coursesRouter = Router();
+
+coursesRouter.post("/signup" , (req, res) => {
+    
+})
+
+coursesRouter.post("/signin" , (req, res) => {
+
+})
+
+coursesRouter.get("/purchases" , (req, res) => {
+
+})
+
+module.exports = {
+    coursesRouter: coursesRouter
+}
